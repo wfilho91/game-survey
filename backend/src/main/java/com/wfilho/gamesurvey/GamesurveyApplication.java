4de563b55be53f4712116e442c14b08396142b9f
wfilho91 <wellington79620@gmail.com>
2021-10-06 08:53:31 -0300

    first commit

---
+++ backend/src/main/java/com/wfilho/gamesurvey/GamesurveyApplication.java
@@ -0,0 +1,13 @@
+package com.wfilho.gamesurvey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamesurveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamesurveyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wfilho/gamesurvey/GamesurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfilho.gamesurvey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
