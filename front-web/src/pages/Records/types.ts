397f033163ef2e5f833de642cd33d4afb1562fde
wfilho91 <wellington79620@gmail.com>
2021-10-14 23:27:06 -0300

    Env var

--- front-web/src/pages/Records/types.ts
+++ front-web/src/pages/Records/types.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.BACKEND_URL ?? 'http://localhost:8080'
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080'
 
 export type RecordsResponse = {
   content: RecordItem []
