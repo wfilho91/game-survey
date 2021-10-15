1db6c9b60e6b91c9c10347dd0b5988c554ca32e2
wfilho91 <wellington79620@gmail.com>
2021-10-14 23:40:10 -0300

    correction in the BASE_URL variable

--- front-web/src/pages/Records/types.ts
+++ front-web/src/pages/Records/types.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080'
+export const BASE_URL = 'https://game-survey-wellington.herokuapp.com'
 
 export type RecordsResponse = {
   content: RecordItem []
